d1aa147a08f9986bd50583499d02296576eba9ef
Veedas <PG5025SP>
2019-12-07 12:51:20 +0200

    Sitas geras

--- Aivaras Ževžikovas 11 testas/Project1/Source.cpp
+++ Aivaras Ževžikovas 11 testas/Project1/Source.cpp
@@ -2,32 +2,47 @@
 #include <fstream>
 
 using namespace std;
-void  kiek(char N[],int K[],int x) {
+
+void  kiek(char N[],int K[],int x) 
+{
 	char s;
+
 	ifstream fd("Tekstas.txt");
-	while (!fd.eof()) {
+
+	while (!fd.eof()) 
+	{
 		fd.get(s);
-		if (s == N[x]) {
+		if (s == N[x]) 
+		{
 			K[x] = K[x] + 1;
 		}
 	}
+
+	fd.close();
 }
 
-void tikrinti(char N[],int K[],int x) {
-	for (char i = 48; i < 58; i++) {
+void tikrinti(char N[],int K[],int x) 
+{
+	for (char i = 48; i < 58; i++) 
+	{
 		N[x] = i;
 		x++;
 	}
-	for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < 10; i++) 
+	{
 		K[i] = 0;
 		kiek(N, K, i);
 	}
 }
 
-void rikiavimas(char N[],int K[]) {
-	for (int i = 0; i < 10; i++){
-		for (int j = i + 1; j < 10; j++){
-			if (K[i] < K[j]){
+void rikiavimas(char N[],int K[]) 
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = i + 1; j < 10; j++)
+		{
+			if (K[i] < K[j])
+			{
 				int temp1 = N[i];
 				N[i] = N[j];
 				N[j] = temp1;
@@ -39,23 +54,35 @@ void rikiavimas(char N[],int K[]) {
 	}
 }
 
-void isvedimas(char N[],int K[]) {
+void isvedimas(char N[],int K[]) 
+{
 	ofstream fr("Rezultatai.txt");
+
 	fr << "Pries rikiavima" << endl;
-	for (int i = 0; i < 10; i++) {
+
+	for (int i = 0; i < 10; i++) 
+	{
 		fr << N[i] << " " << K[i] << endl;
 	}
+
 	rikiavimas(N,K);
+
 	fr << "Po rikiavimo" << endl;
-	for (int i = 0; i < 10; i++) {
+
+	for (int i = 0; i < 10; i++) 
+	{
 		fr << N[i] << " " << K[i] << endl;
 	}
 }
+
 int main() {
 	int K[100], x = 0;
 	char N[100];
+
 	tikrinti(N,K,x);
 	isvedimas(N,K);
+
 	system("pause");
+
 	return 0;
 }
